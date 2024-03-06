20b630c4f8ca2db480eab4bea9945511e3484dbf
Praneeth reddy Kancharakuntla <pkancha2@asu.edu>
2024-03-06 10:44:00 -0700

    updated .jso

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './components/header/Header'
 import About from './components/about/About'
-import Experience from './components/experience/Experience'
 import Skills from './components/skills/Skills'
 import Myprojects from './components/myprojects/Myprojects'
 import Footer from './components/footer/Footer'
@@ -14,7 +13,6 @@ const App = () => {
         <Header/>
         <About/>
         <Skills/>
-        {/* <Experience/> */}
         <Work/>
         <Myprojects/>
         <Footer/>
